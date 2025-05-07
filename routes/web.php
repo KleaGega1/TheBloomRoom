69482358fd58626f7441de616aeba548e5c9c1e7
KleaGega1 <klea.gega@fshnstudent.info>
2025-05-07 23:51:44 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,41 @@ $router->map('GET', '/profile/[i:id]/edit', 'User\ProfileController@edit');
 $router->map('POST', '/profile/[i:id]/update', 'User\ProfileController@update');
 $router->map('GET', '/profile/[i:id]/edit/password', 'User\ProfileController@editPassword');
 $router->map('POST', '/profile/[i:id]/update/password', 'User\ProfileController@updatePassword');
+// User Product Routes
+$router->map('GET', '/products[/]?', 'ProductController@index');
+$router->map('GET', '/products/([0-9]+)[/]?', 'ProductController@show');
 /**
  * ========== Admin routes ==========
  */
 $router->map('GET', '/admin[/]?', 'Admin\DashboardController@index');
+// Admin Product Routes
+$router->map('GET', '/admin/products[/]?', 'Admin\ProductController@index');
+$router->map('GET', '/admin/products/create[/]?', 'Admin\ProductController@create');
+$router->map('POST', '/admin/products/store[/]?', 'Admin\ProductController@store');
+$router->map('GET', '/admin/products/[i:id]/edit/', 'Admin\ProductController@edit');
+$router->map('POST', '/admin/products/[i:id]/update/', 'Admin\ProductController@update');
+$router->map('POST', '/admin/products/[i:id]/delete/', 'Admin\ProductController@delete');
+$router->map('GET', '/admin/products/low-stock[/]?', 'Admin\ProductController@lowStock');
+$router->map('GET', '/admin/products/out-of-stock[/]?', 'Admin\ProductController@outOfStock');
+$router->map('GET', '/admin/products/composition/manage/[i:id]', 'Admin\ProductController@manageComposition');
+$router->map('POST', '/admin/products/composition/update/[i:id]', 'Admin\ProductController@updateComposition');
 
+
+# categories
+$router->map('GET', '/admin/categories[/]?', 'Admin\CategoryController@index');
+$router->map('GET', '/admin/categories/create[/]?', 'Admin\CategoryController@create');
+$router->map('POST', '/admin/categories/store/', 'Admin\CategoryController@store');
+$router->map('GET', '/admin/categories/[i:id]/edit/', 'Admin\CategoryController@edit');
+$router->map('POST', '/admin/categories/[i:id]/update/', 'Admin\CategoryController@update');
+$router->map('POST', '/admin/categories/[i:id]/delete/', 'Admin\CategoryController@delete');
+
+# gifts
+$router->map('GET', '/admin/gifts[/]?', 'Admin\GiftController@index');
+$router->map('GET', '/admin/gifts/create[/]?', 'Admin\GiftController@create');
+$router->map('POST', '/admin/gifts/store', 'Admin\GiftController@store');
+$router->map('GET', '/admin/gifts/[i:id]/edit/', 'Admin\GiftController@edit');
+$router->map('POST', '/admin/gifts/[i:id]/update/', 'Admin\GiftController@update');
+$router->map('POST', '/admin/gifts/[i:id]/delete/', 'Admin\GiftController@delete');
 /**
  * ========== Forgot Password routes ==========
  */
@@ -29,3 +59,17 @@ $router->map('GET','/forgot-password', 'PasswordResetController@showForgotForm')
 $router->map('POST','/forgot-password', 'PasswordResetController@sendResetLink');
 $router->map('GET','/reset-password[/]?', 'PasswordResetController@showResetForm');
 $router->map('POST','/reset-password', 'PasswordResetController@resetPassword');
+
+# categories routes
+$router->map('GET', '/categories/[i:id][/]?', 'CategoryController@show');
+
+
+# products routes
+$router->map('GET', '/products[/]?', 'ProductController@index');
+
+$router->map('GET', '/products/[i:id][/]?', 'ProductController@show');
+
+$router->map('GET', '/gifts[/]?', 'GiftController@index');
+$router->map('GET', '/gifts/[i:id][/]?', 'GiftController@show');
+$router->map('GET', '/gifts/category/[*:categoryName]', 'GiftController@category');
+$router->map('GET', '/categories/[*:slug][/]?', 'CategoryController@showByCategorySlug');
